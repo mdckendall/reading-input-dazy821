0197624334edcf174cacee418d96920d6237a7f0
Kayla Ribeiro <>
2023-02-26 04:12:21 +0000

    Made the program read the length of a word

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Enter a word:");
+    Scanner sc = new Scanner(System.in);
     
-    //Your code will return the length of the entered String
+    System.out.println("Enter a word:");
+    String word = sc.nextLine();
+
+    int length= word.length();
+    System.out.println(length);
+    
   }
 }
